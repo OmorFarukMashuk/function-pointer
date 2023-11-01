b7173edc3258fe961bfa28091814d05e2f5f8185
Omor Faruk Mashuk <mofaruk@uwaterloo.ca>
2023-10-31 21:44:53 -0400

    updated function

--- fp.cpp
+++ fp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
@@ -11,32 +12,45 @@ bool descendingCompare(int a, int b)
     return a < b;
 }
 
-int main()
+
+
+// void bubbleSort(int *arr, bool (*compareFuncPtr)(int, int))
+// {
+//     for (int i = 0; i < getArrLen(arr); i++)
+//     {
+//         for (int j = 0; j < getArrLen(arr) - i - 1; j++)
+//         {
+//             if (compareFuncPtr(arr[j], arr[j + 1]))
+//             {
+//                 swap(arr[j], arr[j + 1]);
+//             }
+//         }
+//     }
+
+//     return;
+// }
+
+void showList(vector<int> arr)
 {
-    int arr[] = {6, 5, 4, 3, 2};
-    int arrLen = end(arr) - begin(arr);
-
-
-    //function pointer 
-    bool (*compareFuncPtr)(int, int) = descendingCompare;
-
-    for (int i = 0; i < arrLen; i++)
-    {
-        for (int j = 0; j < arrLen - i - 1; j++)
-        {
-            if (compareFuncPtr(arr[j], arr[j + 1]))
-            {
-                swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-
     cout << "sorted list:  ";
 
-    for (int i = 0; i < arrLen; i++)
+    for (int i = 0; i < arr.size(); i++)
     {
         cout << arr[i] << " ";
     }
+}
+
+int main()
+{
+    vector<int> arr{6, 5, 4, 3, 2};
+
+
+    // function pointer
+    // bool (*compareFuncPtr)(int, int) = descendingCompare;
+
+    // bubbleSort(arr, compareFuncPtr);
+
+    showList(arr);
 
     return 0;
 }
